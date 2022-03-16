2663d7c8b8a85d2d8d8c1e6a3c5a7500a816e841
IluminEXQ <70211741+IluminEXQ@users.noreply.github.com>
2022-03-16 10:19:52 +0100

    first

---
+++ umm/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const databaseModule = require('./databasemodule.js')
+const app = express()
+const port = 1337
+
+const clientDir = __dirname + "\\client\\"
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static(clientDir))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {name : "Ilumin"})
+})
+  
+  app.post('/sendMessage', function (req, res) {
+    console.log(req.body.name)
+    console.log(req.body.message)
+    
+   databaseModule.saveMessage(req.body.name, req.body.message)
+   
+
+    res.redirect('/success')
+})
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
---
+++ umm/functions.js
@@ -0,0 +1,9 @@
+console.log("----------")
+
+function skrivUtSumman(a, b){
+    console.log(a + b);
+}
+
+console.log("----------")
+
+skrivUtSumman(1, 2)
